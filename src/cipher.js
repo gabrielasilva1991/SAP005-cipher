6afe0b06d0961eac9608a80167b6b515066ce5ad
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-10-28 10:10:24 -0200

    Alterações na estrutura do html, idéia inicial para a função e no css coloquei imagem dei uma organizada e formatada.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
-const cipher = {
-  // ...
-};
+const cipher = function encrypt (mensagem, deslocamento) {
+  let codigoDaLetraASC = 65
+  let cod1aLetra = -65
+  let desloc = 4
+  let tamDoAlfabeto = 26
+
+
+  if (mensagem == ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra)
+  return mensagem.fromCharCode();
+
+}
+ 
+   // ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+   // return string.fromCharCode();
+
+
 
 export default cipher;
+
+
+
+ // function (decrypt) {
+   // ((codigoDaLetraASC - cod1aLetra - desloc) % tamDoAlfabeto) + cod1aLetra
+   // return string.fromCharCode();
+
+//};
+
+
+
+
+
+
+
+// ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+//     65 a 90             -65          4             26          65
+// codigoAsc ==>     codigo0a25 ==> deslocamento ==> giro ==> codigoASC
+
+// ((codigoDaLetraASC - cod1aLetra - desloc) % tamDoAlfabeto) + cod1aLetra
