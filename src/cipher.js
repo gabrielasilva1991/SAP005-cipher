8c0abb88fadf15fc56f8e9a40712fb22a15c2923
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2020-11-05 22:32:49 -0200

    javascript finalizado (index e cipher), Html melhorado, falta melhorar o css. Teste executado deu erros, falta adicionar typeerror.

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,64 @@
-const cipher = 
+const cipher = {
 
-function criptografarMensagem (mensagem, deslocamento) {
-  let mensagemDigitada = []
-  let numDeslocamento;
+  encode: function (mensagemDigitada, numDeslocamento) {
+    let mensagemRetornada = "";
+    let tamAlfabeto = 26;
+    let codigoAscii = 65;
 
-  for (let i = 0; i < mensagem.length; i++) {
-    numDeslocamento = mensagem.charCodeAt([i]);
+    for (let i = 0; i < mensagemDigitada.length; i++) {
+      let posicaoInicialMensagem = Number(mensagemDigitada.charCodeAt[i]);
+      let posicaoFinalMensagem = ((posicaoInicialMensagem - codigoAscii + numDeslocamento) % tamAlfabeto) + codigoAscii;
+      let mensagemFinal = String.fromCharCode(posicaoFinalMensagem);
+  
+      mensagemRetornada += mensagemFinal     
+          
+      }
+      
+      return mensagemRetornada;
+
+    }
+      
+  },
+
+  decode: function (mensagemDigitada, numDeslocamento) {
+    let mensagemRetornada = "";
+    let tamAlfabeto = 26;
+    let codigoAscii = 65;
+
+    for (let i = 0; i < mensagemDigitada.length; i++) {
+      let posicaoInicialMensagem = Number(mensagemDigitada.charCodeAt[i]);
+      let posicaoFinalMensagem = ((posicaoInicialMensagem + codigoAscii - numDeslocamento) % tamAlfabeto) + codigoAscii;
+      let mensagemFinal = String.fromCharCode(posicaoFinalMensagem);  
+
+      mensagemRetornada += mensagemFinal 
+
+    }
+
+    return mensagemRetornada;
+    
   }
-
+ 
 }
-  return encodeURIComponent("mensagemCifrada")
-
-function descriptografarMensagem (mensagem, deslocamento) {
-  for (let i = 0; i < mensagem.length; i++) {
-    numDeslocamento = mensagem.charCodeAt([i]);
-  }
-
-
-}
-return decodeURIComponent("mensagemDecifrada")
-
-
 
 export default cipher;
 
 
-// let codigoDaLetraASC = 65
-// let cod1aLetra = -65
-// let desloc = [4]
-// let Alfabeto = [26]
 
-// function (decrypt)
-// return string.fromCharCode();
+//if (!mensagemDigitada || !numDeslocamento) {
+//throw new TypeError("Inserir a mensagem e o deslocamento ");
 
 
+
+//se não digitar mensagem OU deslocamento o codigo não executa ==> if (!mensagemDigitada || !numDeslocamento)
+
+//charCodeAt[i] ==> pega a posição de cada caractere da mensagem em forma de número
+
+//String.fromCharcode ==> retorna a mensagem em forma de texto
+
+
+//condição inicial // passar pela letra // repete para cada letra
+//for (let i = 0; i < mensagemDigitada.length; i++) {}
 
 // ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
-//     65 a 90             -65          4             26          65
+//     65 a 90             -65                        26          65
 // codigoAsc ==>     codigo0a25 ==> deslocamento ==> giro ==> codigoASC
-
-// ((codigoDaLetraASC - cod1aLetra - desloc) % tamDoAlfabeto) + cod1aLetra
--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 import cipher from './cipher.js';
 
 
-console.log(cipher);
 
-let numDeslocamento = parseInt(document.getElementById("numDeslocamento").value) 
-let mensagemDigitada = document.getElementById ("mensagemDigitada").value;
-let criptografarMensagem = cipher.encodeURIComponent ("mensagemCifrada")
-let mensagemRetornada = document.getElementById ("mensagemRetornada").value;
-let descriptografarMensagem = cipher.decodeURIComponent ("mensagemDecidrada")
+document.getElementById("Criptografar").addEventListener ("click", criptografarMensagem);
+
+function criptografarMensagem () {
+    
+    let mensagemDigitada = String(document.getElementById ("mensagemDigitada").value).toUpperCase(); 
+    
+    let numDeslocamento = Number(document.getElementById("numDeslocamento").value); 
+      
+    let criptografar = cipher.encode(mensagemDigitada, numDeslocamento); 
+
+    document.getElementById("mensagemRetornada").value = criptografar
+
+}
 
 
 
-// Copiar mensagem de uma caixa de texto  texto para outra:
-//document.getElementById("mensagemDigitada").value = document.getElementById("mensagemRetornada").value;
+document.getElementById("Descriptografar").addEventListener ("click", descriptografarMensagem);
 
-//Janela 1 : Digite aqui a mensagem que deseja enviar
-//Janela 2: Mensagem retornando criptografada
+function descriptografarMensagem () {
+
+    let mensagemDigitada = String(document.getElementById ("mensagemDigitada").value).toUpperCase(); 
+    
+    let numDeslocamento = Number(document.getElementById("numDeslocamento").value); 
+    
+    let descriptografar = cipher.decode(mensagemDigitada, numDeslocamento); 
+
+    document.getElementById("mensagemRetornada").value = descriptografar
+
+}
+
+
+
+// document.getElementById("Criptografar").addEventListener ("click", function(event) {
+//     event.preventDefault()
+//     criptografar();
+
+// const btnCriptografar //mensagem retornada criptografada
+// let mensagemDigitada //mensagem digitada deve retornar em forma de texto com as letras maiusculas
+// let numDeslocamento //numero do deslocamento para ser escolhido no formato de número
+// let criptografar //criptografar mensagem e disponibilizar para o html
